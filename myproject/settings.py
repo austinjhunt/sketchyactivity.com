c0f7b572d7f19dc7b1d79163cf9638543b9a1d65
Austin Hunt <huntaj@g.cofc.edu>
2020-08-03 12:44:04 -0400

    use an s3 bucket for media file uploads/storage

--- myproject/settings.py
+++ myproject/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'compressor',
     'cssmin',
     'jsmin',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -160,8 +161,23 @@ COMPRESS_OFFLINE = True
 COMPRESS_OUTPUT_DIR = 'sketchyactivity' # hashed output to main/static/main folder instead of main/static/CACHE
 COMPRESS_CSS_FILTERS = ["compressor.filters.cssmin.CSSMinFilter"]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = '/home/huntajoseph/webapps/sketchyactivity_media' #  os.path.join(BASE_DIR, 'sketchyactivity/media')
-print(MEDIA_ROOT)
+USE_S3 = os.getenv('USE_S3') == 'TRUE'
+if USE_S3: 
+    print("USE s3")
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'sketchyactivity.storage_backends.PublicMediaStorage'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+#MEDIA_ROOT = '/home/huntajoseph/webapps/sketchyactivity_media' #  os.path.join(BASE_DIR, 'sketchyactivity/media')
+
 
 django_heroku.settings(locals())
