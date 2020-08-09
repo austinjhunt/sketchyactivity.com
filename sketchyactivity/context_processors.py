b5246b7f20fbb4bbc5b7c893e2582c190ff39235
Austin Hunt <huntaj@cofc.edu>
2020-08-08 22:29:27 -0400

    preview image dynamic

--- sketchyactivity/context_processors.py
+++ sketchyactivity/context_processors.py
@@ -1,4 +1,4 @@
 from .models import *
 def s3context(request):
-    main_preview_image = PortfolioItem.objects.get(filename="jennalynn.JPG").s3_drawing_private_url
+    main_preview_image = PortfolioItem.objects.all()[-1].s3_drawing_private_url
     return {'base_media_url':'','preview_image': main_preview_image}
