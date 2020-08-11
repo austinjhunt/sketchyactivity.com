7a5b2c410fb45f865588b6b24d40e588fe7c1856
Austin Hunt <huntaj@g.cofc.edu>
2020-08-11 00:28:34 -0400

    attempt 2 to fix animated video scrolling

--- myproject/settings.py
+++ myproject/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 
