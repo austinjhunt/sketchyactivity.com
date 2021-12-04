4e9723bc1c69979317b485717ae113d579b941e6
Austin Hunt <huntaj@g.cofc.edu>
2021-12-03 23:29:02 -0500

    change DEBUG to False

--- myproject/settings.py
+++ myproject/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '__d%ysb=p9yjd2(zkn!fzlk95)swp&=lp!lr-ws+cqqxqx$6uk'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['www.sketchyactivity.com','sketchyactivity.com','127.0.0.1','localhost']
 
 INSTALLED_APPS = [
