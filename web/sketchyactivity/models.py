01e9316630819c2419d20267d61b2a20bec73d7b
Austin Hunt <austinsketches@gmail.com>
2023-01-19 15:08:29 -0500

    use promo_message_html as model variable

--- web/sketchyactivity/models.py
+++ web/sketchyactivity/models.py
@@ -9,38 +9,43 @@ import boto3
 from botocore.client import Config
 from django.conf import settings
 import datetime
-import logging 
+import logging
 logger = logging.getLogger('sketchyactivity')
 
+
 class PortfolioItem(models.Model):
-    tag = models.CharField(default='',max_length=100)
-    filename = models.CharField(default='',max_length=100)
-    portrait_name = models.CharField(default='',max_length=100)
+    tag = models.CharField(default='', max_length=100)
+    filename = models.CharField(default='', max_length=100)
+    portrait_name = models.CharField(default='', max_length=100)
     date = models.DateField(null=True)
     # Each item stored twice. One big, one small copy for preview. Need private url for each.
-    s3_drawing_private_url = models.URLField(max_length=500,default="")
-    s3_copied_smaller_drawing_private_url = models.URLField(max_length=500,default="")
+    s3_drawing_private_url = models.URLField(max_length=500, default="")
+    s3_copied_smaller_drawing_private_url = models.URLField(
+        max_length=500, default="")
+
 
 class MetaStuff(models.Model):
     bio = models.TextField(default='')
     profile_image_filename = models.CharField(default='', max_length=100)
     profile_image_private_url = models.URLField(max_length=500, default="")
-    website_title = models.CharField(max_length=128,default="")
+    website_title = models.CharField(max_length=128, default="")
     website_description = models.TextField(default="")
     website_keywords = models.TextField(default="")
     sale = models.BooleanField(default=False)
     sale_amount = models.FloatField(default=0)
     sale_start = models.DateField()
     sale_end = models.DateField()
+    promo_message_html = models.TextField(default='')
 
     def sale_still_active(self):
         # current date is on or after sale_start and before or on sale_end
         today = datetime.datetime.today().date()
-        return self.sale and today >= self.sale_start and today <= self.sale_end # sale active
+        return self.sale and today >= self.sale_start and today <= self.sale_end  # sale active
 
     def get_sale_price(self, original_price):
         """ return the sale price of an item given original price """
         return round(original_price * (1 - self.sale_amount), 2)
+
 
 class Product(models.Model):
     """ Products that a user adds to their cart """
@@ -49,7 +54,8 @@ class Product(models.Model):
     type = models.TextField()
     price = models.FloatField()
     reference_image_filename = models.CharField(default='', max_length=100)
-    s3_reference_image_url = models.URLField(max_length=500,default="")
+    s3_reference_image_url = models.URLField(max_length=500, default="")
+
     class CommissionStatus(models.TextChoices):
         NOT_STARTED = 'NS', _('Not Started Yet')
         IN_PROGRESS = 'IP', _('In Progress')
@@ -59,14 +65,14 @@ class Product(models.Model):
         choices=CommissionStatus.choices,
         default=CommissionStatus.NOT_STARTED
     )
-    completion_date = models.DateField(null=True,blank=True)
+    completion_date = models.DateField(null=True, blank=True)
 
- 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=20)
     cart = models.ManyToManyField(Product)
+
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
@@ -77,12 +83,16 @@ class UserProfile(models.Model):
         print(instance.__dict__)
         instance.userprofile.save()
 
+
 class Purchase(models.Model):
     """ keep record of purchase """
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_created=True) # save as now when purchase created
-    test = models.BooleanField(default=False) # indicate whether this was used for testing
+    # save as now when purchase created
+    timestamp = models.DateTimeField(auto_created=True)
+    # indicate whether this was used for testing
+    test = models.BooleanField(default=False)
+
 
 class Price(models.Model):
     amount = models.FloatField(default=0)
@@ -90,6 +100,7 @@ class Price(models.Model):
 
     size = models.CharField(max_length=100, default='8.5x12in')
     num_subjects = models.IntegerField(default=1)
+
     class DrawingStyle(models.TextChoices):
         TRADITIONAL = 'TR', _('Traditional')
         DIGITAL = 'DI', _('Digital')
@@ -101,10 +112,12 @@ class Price(models.Model):
     )
 
 ## Receivers / listeners
+
+
 @receiver(models.signals.post_delete, sender=Product)
 def auto_delete_commission_reference_image_on_product_delete(sender, instance, **kwargs):
-    """ When product is deleted, automatically use S3 client to delete S3 file corresponding 
-    to product (the reference image) """ 
+    """ When product is deleted, automatically use S3 client to delete S3 file corresponding
+    to product (the reference image) """
     s3_client = boto3.client(
         's3',
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
@@ -113,16 +126,17 @@ def auto_delete_commission_reference_image_on_product_delete(sender, instance, *
         config=Config(signature_version='s3v4'))
     try:
         s3_client.delete_object(
-            Bucket='sketchyactivitys3', 
+            Bucket='sketchyactivitys3',
             Key=f'media/commission-reference-images/{instance.reference_image_filename}'
         )
-    except Exception as e: 
+    except Exception as e:
         logger.error(str(e))
+
 
 @receiver(models.signals.post_delete, sender=PortfolioItem)
 def auto_delete_image_files_on_portfolio_item_delete(sender, instance, **kwargs):
-    """ When product is deleted, automatically use S3 client to delete S3 file corresponding 
-    to product (the reference image) """ 
+    """ When product is deleted, automatically use S3 client to delete S3 file corresponding
+    to product (the reference image) """
     s3_client = boto3.client(
         's3',
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
@@ -131,15 +145,15 @@ def auto_delete_image_files_on_portfolio_item_delete(sender, instance, **kwargs)
         config=Config(signature_version='s3v4'))
     try:
         s3_client.delete_object(
-            Bucket='sketchyactivitys3', 
+            Bucket='sketchyactivitys3',
             Key=f'media/drawings/{instance.filename}'
         )
-    except Exception as e: 
+    except Exception as e:
         logger.error(str(e))
     try:
         s3_client.delete_object(
-            Bucket='sketchyactivitys3', 
+            Bucket='sketchyactivitys3',
             Key=f'media/copied_smaller_drawings/{instance.filename}'
         )
-    except Exception as e: 
+    except Exception as e:
         logger.error(str(e))
