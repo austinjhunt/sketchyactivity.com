9fd4a8cb5f1b5f9fb74e3e7523628582c3b11d2c
Austin Hunt <austinsketches@gmail.com>
2024-05-19 13:10:16 -0400

    remove docstring with private url

--- sketchyactivitybackend/migrate_to_squarespace.py
+++ sketchyactivitybackend/migrate_to_squarespace.py
@@ -22,17 +22,6 @@ class SquareSpaceProjectUploader:
         pass
 
     def get_projects(self):
-        """return list of projects each with structure
-
-        {
-            "id": 32,
-            "tag": "Portrait",
-            "filename": "brbawalt.JPG",
-            "portrait_name": "Walt",
-            "date": "2014-01-01",
-            "s3_drawing_private_url": "https://sketchyactivitys3.s3.amazonaws.com/media/drawings/brbawalt.JPG?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWDTAGLLHXMOED4H7%2F20240517%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20240517T105152Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=ed27946195eba2927be8806b040e7b8dc4d9f6fb991603ab29f3e79e2b742f0a",
-            "s3_copied_smaller_drawing_private_url": "https://sketchyactivitys3.s3.amazonaws.com/media/copied_smaller_drawings/brbawalt.JPG?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWDTAGLLHXMOED4H7%2F20240517%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20240517T105152Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=bf02e5bbc1888160bd3a91404467c981784f0d39978690b65bd30c34b15fc3b0"
-        }
-        """
+        """return list of projects each with structure"""
         with open("porfolio.json", "r") as f:
             return json.load(f)
